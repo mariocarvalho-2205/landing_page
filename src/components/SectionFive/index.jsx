349a862753a45dbb4bb87452282ba51ad5945a31
mario carvalho <mario.carvalho.devpython@gmail.com>
2024-02-10 12:12:36 -0300

    section five add balls

--- src/components/SectionFive/index.jsx
+++ src/components/SectionFive/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ArrowBig from "../../assets/arrowbig.svg";
+import LineH from "../../assets/line_section_five.svg"
 import './styles.css'
 
 
 
 const SectionFive = () => {
   return (
-    <div className="sectionFive">
+    <div className="sectionFive_container">
       <div className="sectionFive_top">
         <div className="sectionFive_text">
-          <h2>WHY BUILD</h2>
+          <h2>HOW WE BUILD</h2>
           <div className="h2_img">
-            <h2>WITH HYDRA?</h2>
+            <h2>WITH HYDRA VR?</h2>
             <img src={ArrowBig} alt="" />
           </div>
         </div>
@@ -23,6 +24,29 @@ const SectionFive = () => {
           Lectus magna fringilla urna porttitor rhoncus vitae.
         </p>
       </div>
+
+      <div className='sectionFive_Bottom'>
+        <div className='line_balls'>
+          <div className='lineH'>
+            <img src={LineH} alt="" />
+          </div>
+          <div className="balls_Section_Five">
+            <div className='ball_Sec_Five'>
+              <h2>01</h2>
+            </div>
+            <div className='ball_Sec_Five'>
+              <h2>02</h2>
+            </div>
+            <div className='ball_Sec_Five'>
+              <h2>03</h2>
+            </div>
+            <div className='ball_Sec_Five'>
+              <h2>04</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
