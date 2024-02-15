57d2aa625675fc957157a41f35b556430272006d
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-02-14 21:48:37 -0300

    create form

--- src/components/SectionSix/index.jsx
+++ src/components/SectionSix/index.jsx
@@ -1,30 +1,141 @@
-import React from 'react'
-import Line from "../../assets/line_Horizontal.svg"
-import './styles.css'
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import Line from "../../assets/line_Horizontal.svg";
+import "./styles.css";
 
 const SectionSix = () => {
-  return (
-    <div className='sectionSix_Container'>
-      <div className='sectionSix_Title_Div'>
-        <h2 className='sectionSix_title_H2'>Join Hydra</h2>
-        <img src={Line} alt="line_separation" />
-        <h3 className='sectionSix_SubTitle_H2'>Let’s Build Your VR Experience</h3>
-      </div>
-      <div className='sectionSix_form_container'>
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-        <form className='sectionSix_Form'>
-          <input className='input_form firstName' type="text" id='first_name' name='first_name' placeholder='First Name'/>
-          <input className='input_form lastName' type="text" id='last_name' name='last_name' placeholder='Last Name'/>
-          <input className='input_form email' type="text" id='email' name='email' placeholder='Email'/>
-          <input className='input_form phoneNumber' type="text" id='phone_number' name='phone_number' placeholder='Phone Number'/>
-          <input className='input_form subject' type="text" id='subject' name='subject' placeholder='Subject'/>
-          <textarea className='textarea' name="textarea" id="textarea" cols="30" rows="10" placeholder='Tell Us Something...'></textarea>
-          <button className='button button_big'>Send to Hydra</button>
+	const sendData = async (data) => {
+		try {
+			// const response = await axios.post('http://localhost:3000/api/formData', data)
+			// console.log(response.data)
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-        </form>
-      </div>
-    </div>
-  )
-}
+	const onSubmit = (data) => {
+		sendData(data);
+	};
 
-export default SectionSix
+	return (
+		<div className="sectionSix_Container" onSubmit={handleSubmit(onSubmit)}>
+			<div className="sectionSix_Title_Div">
+				<h2 className="sectionSix_title_H2">Join Hydra</h2>
+				<img src={Line} alt="line_separation" />
+				<h3 className="sectionSix_SubTitle_H2">
+					Let’s Build Your VR Experience
+				</h3>
+			</div>
+			<div className="sectionSix_form_container">
+				<form className="sectionSix_Form">
+					<input
+						className="input_form firstName"
+						type="text"
+						id="first_name"
+						name="first_name"
+						placeholder="First Name"
+						{...register("first_name", {
+							/*{ required: true, minLength: 3 }*/
+						})}
+					/>
+					{/* {errors.first_name && errors.first_name.type === "required" && (
+						<p>This field is required</p>
+					)}
+					{errors.first_name && errors.first_name.type === "minLength" && (
+						<p>Minimum length is 3 characters</p>
+					)} */}
+					<input
+						className="input_form lastName"
+						type="text"
+						id="last_name"
+						name="last_name"
+						placeholder="Last Name"
+						{...register("last_name", {
+							/*{ required: true, minLength: 3 }*/
+						})}
+					/>
+					{/* {errors.last_name && errors.last_name.type === "required" && (
+						<p>This field is required</p>
+					)}
+					{errors.last_name && errors.last_name.type === "minLength" && (
+						<p>Minimum length is 3 characters</p>
+					)} */}
+					<input
+						className="input_form email"
+						type="text"
+						id="email"
+						name="email"
+						placeholder="Email"
+						{...register("email", {
+							/*{ required: true, pattern: /^\S+@\S+$/i }*/
+						})}
+					/>
+					{/* {errors.email && errors.email.type === "required" && (
+						<p>This field is required</p>
+					)}
+					{errors.email && errors.email.type === "pattern" && (
+						<p>Invalid email address</p>
+					)} */}
+					<input
+						className="input_form phoneNumber"
+						type="text"
+						id="phone_number"
+						name="phone_number"
+						placeholder="Phone Number"
+						{...register("phone_number", {
+							/*{ required: true, pattern: /^\d{10}$/ }*/
+						})}
+					/>
+					{/* {errors.phone_number && errors.phone_number.type === "required" && (
+						<p>This field is required</p>
+					)}
+					{errors.phone_number && errors.phone_number.type === "pattern" && (
+						<p>Invalid phone number</p>
+					)} */}
+					<input
+						className="input_form subject"
+						type="text"
+						id="subject"
+						name="subject"
+						placeholder="Subject"
+						{...register("subject", {
+							/*{ required: true, minLength: 5 }*/
+						})}
+					/>
+					{/* {errors.subject && errors.subject.type === "required" && (
+						<p>This field is required</p>
+					)}
+					{errors.subject && errors.subject.type === "minLength" && (
+						<p>Minimum length is 5 characters</p>
+					)} */}
+					<textarea
+						className="textarea"
+						name="something"
+						id="something"
+						cols="30"
+						rows="10"
+						placeholder="Tell Us Something..."
+						{...register("something", {
+							/*{ required: true }*/
+						})}
+					></textarea>
+					{/* {errors.something && errors.something.type === "required" && (
+						<p>This field is required</p>
+					)} */}
+					<button type="submit" className="button button_big">
+						Send to Hydra
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default SectionSix;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Define a pasta raiz de onde os arquivos estáticos serão servidos
+    root: './src',
+  },
+});
