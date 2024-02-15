1f0cde0fab621d02cded5ed1c6e0758435e90b76
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-02-15 19:12:08 -0300

    create login page and styled finlally

--- client/src/components/LoginPage/index.jsx
+++ client/src/components/LoginPage/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./styles.css"
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+	return (
+    <>
+		<div className="login_Container">
+      <h1>Login</h1>
+      
+			<form className="login_form">
+					<input type="text" name="user" placeholder="User"/>
+					<input type="text" name="password" placeholder="Password"/>
+        <button className="button button_small" type="submit">Login</button>
+			</form>
+		</div>
+    </>
+	);
+};
 
-export default LoginPage
+export default LoginPage;
