b5d36f21b9805876a33c270f12ba2ef510e21215
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-02-17 09:10:50 -0300

    create button to home

--- client/src/components/LoginPage/index.jsx
+++ client/src/components/LoginPage/index.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import "./styles.css"
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 const LoginPage = () => {
 	return (
-    <>
-		<div className="login_Container">
-      <h1>Login</h1>
-      
-			<form className="login_form">
-					<input type="text" name="user" placeholder="User"/>
-					<input type="text" name="password" placeholder="Password"/>
-        <button className="button button_small" type="submit">Login</button>
-			</form>
-		</div>
-    </>
+		<>
+			<div className="login_Container">
+				<h1>Login</h1>
+
+				<form className="login_form">
+					<input type="text" name="user" placeholder="User" />
+					<input type="text" name="password" placeholder="Password" />
+					<button className="button button_small" type="submit">
+						Login
+					</button>
+					<Link to="/">
+						<button className="button button_small" type="submit">
+							Voltar
+						</button>
+					</Link>
+				</form>
+			</div>
+		</>
 	);
 };
 
